f450a391f0b39d54d3c3201e76749b3ddec175b4
Doug Sanders <dougsandersca@gmail.com>
2025-07-20 21:23:10 -0700

    index

--- launchRMD.R
+++ launchRMD.R
@@ -1,2 +1,2 @@
-setwd("~/ritmo_repositories/ritmortools")
+setwd("~/ritmo_repositories/rFCsite")
 rmarkdown::render("GetStarted.Rmd", output_file = "index.html", output_options = list(self_contained = TRUE))
